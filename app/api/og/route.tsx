f88491983f1ad6bf269eff2e0b2f48d67a6c6b91
Satindar <ibrahimasif711@outlook.com>
2023-05-13 13:31:02 +0530

    e

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,66 +1,62 @@
-import { ImageResponse } from '@vercel/og';
-import Image from 'next/image';
-import { NextRequest } from 'next/server';
-import { Blog } from '@/interfaces/Blog';
+import { ImageResponse } from "@vercel/og";
+import Image from "next/image";
+import { NextRequest } from "next/server";
+import { Blog } from "@/interfaces/Blog";
 
-export const config = {
-  runtime: 'experimental-edge',
-};
+export const runtime = "edge";
 
-
-
-export function GET<Props>(request: NextRequest, blog: Blog ) {
+export function GET<Props>(request: NextRequest, blog: Blog) {
   try {
     const { searchParams } = new URL(request.url);
- 
+
     // ?title=<title>
-    const hasTitle = searchParams.has('title');
+    const hasTitle = searchParams.has("title");
     const title = hasTitle
-      ? searchParams.get('title')?.slice(0, 100)
+      ? searchParams.get("title")?.slice(0, 100)
       : blog.title;
- 
+
     return new ImageResponse(
       (
         <div
           style={{
-            backgroundColor: 'black',
-            backgroundSize: '150px 150px',
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            textAlign: 'center',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            flexWrap: 'nowrap',
+            backgroundColor: "black",
+            backgroundSize: "150px 150px",
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            flexWrap: "nowrap",
           }}
         >
           <div
             style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              justifyItems: 'center',
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              justifyItems: "center",
             }}
           >
             <Image
               alt="Author or cover image"
               height={200}
               src={blog.authorImage ?? blog.coverImage}
-              style={{ margin: '0 30px' }}
+              style={{ margin: "0 30px" }}
               width={232}
             />
           </div>
           <div
             style={{
               fontSize: 60,
-              fontStyle: 'normal',
-              letterSpacing: '-0.025em',
-              color: 'white',
+              fontStyle: "normal",
+              letterSpacing: "-0.025em",
+              color: "white",
               marginTop: 30,
-              padding: '0 120px',
+              padding: "0 120px",
               lineHeight: 1.4,
-              whiteSpace: 'pre-wrap',
+              whiteSpace: "pre-wrap",
             }}
           >
             {title}
@@ -70,7 +66,7 @@ export function GET<Props>(request: NextRequest, blog: Blog ) {
       {
         width: 1200,
         height: 630,
-      },
+      }
     );
   } catch (e: any) {
     console.log(`${e.message}`);
@@ -78,5 +74,4 @@ export function GET<Props>(request: NextRequest, blog: Blog ) {
       status: 500,
     });
   }
-  
 }
