e0ff49aab9c70685f054da88c8bba9d91482b5cf
Satindar <ibrahimasif711@outlook.com>
2023-05-16 17:06:09 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,32 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Script from "next/script";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Example Blog',
-  description: 'An exmaple personal blog for you to get the basic idea of how to make one.',
-}
+  title: "Example Blog",
+  description:
+    "An exmaple personal blog for you to get the basic idea of how to make one.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <Script id="clarity" type="text/javascript">
+        {`
+  (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "h4dozsmjb0");
+`}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
