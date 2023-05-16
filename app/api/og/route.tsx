c488b04a77131a135f9d82f39a0d06b6a2012edd
Satindar <ibrahimasif711@outlook.com>
2023-05-16 17:14:17 +0530

    better og:image api

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -6,72 +6,26 @@ import { Blog } from "@/interfaces/Blog";
 export const runtime = "edge";
 
 export function GET<Props>(request: NextRequest, blog: Blog) {
-  try {
-    const { searchParams } = new URL(request.url);
-
-    // ?title=<title>
-    const hasTitle = searchParams.has("title");
-    const title = hasTitle
-      ? searchParams.get("title")?.slice(0, 100)
-      : blog.title;
-
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            backgroundColor: "black",
-            backgroundSize: "150px 150px",
-            height: "100%",
-            width: "100%",
-            display: "flex",
-            textAlign: "center",
-            alignItems: "center",
-            justifyContent: "center",
-            flexDirection: "column",
-            flexWrap: "nowrap",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              justifyItems: "center",
-            }}
-          >
-            <Image
-              alt="Author or cover image"
-              height={200}
-              src={blog.authorImage ?? blog.coverImage}
-              style={{ margin: "0 30px" }}
-              width={232}
-            />
-          </div>
-          <div
-            style={{
-              fontSize: 60,
-              fontStyle: "normal",
-              letterSpacing: "-0.025em",
-              color: "white",
-              marginTop: 30,
-              padding: "0 120px",
-              lineHeight: 1.4,
-              whiteSpace: "pre-wrap",
-            }}
-          >
-            {title}
-          </div>
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      }
-    );
-  } catch (e: any) {
-    console.log(`${e.message}`);
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    });
-  }
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Example personal blog
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+    },
+  );
 }
